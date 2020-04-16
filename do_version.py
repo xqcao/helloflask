b8d9511d93460978550c2ca31304b71877fdbb81
xqcao <xqcaomail@gmail.com>
2020-04-16 00:20:08 -0700

    jenkins version, add py code

---
+++ do_version.py
@@ -0,0 +1,17 @@
+import json
+
+with open('commitflask.json', 'r') as ff1:
+    data = json.load(ff1)
+
+with open("allv.json", 'r') as ff2:
+    data2 = json.load(ff2)
+ii = 1
+for dd in data:
+    try:
+        data2[dd['sha']]
+    except:
+        data2[dd['sha']] = ii
+    ii += 1
+
+with open("allv.json", "w") as ff3:
+    json.dump(data2, ff3)
